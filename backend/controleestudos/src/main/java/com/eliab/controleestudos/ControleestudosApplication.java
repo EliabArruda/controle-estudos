085244da9e60e4d40a59d21c42f7d34578ff58de
Eliab <eliabad20t@gmail.com>
2025-05-12 17:40:11 -0300

    Configura estrutura inicial do projeto Java com Spring Boot

---
+++ backend/controleestudos/src/main/java/com/eliab/controleestudos/ControleestudosApplication.java
@@ -0,0 +1,13 @@
+package com.eliab.controleestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleestudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleestudosApplication.class, args);
+	}
+
+}
---
+++ backend/controleestudos/src/test/java/com/eliab/controleestudos/ControleestudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliab.controleestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleestudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
