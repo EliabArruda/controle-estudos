fa9e0ccb1ec214a6258057d3f806ec6dc7880c5b
Eliab <eliabad20t@gmail.com>
2025-05-15 13:11:39 -0300

    CrImplementacao da camada 'Repository'

--- backend/controleestudos/src/main/java/com/eliab/controleestudos/repository/HistoricoRepository.java
+++ backend/controleestudos/src/main/java/com/eliab/controleestudos/repository/HistoricoRepository.java
@@ -1,2 +1,9 @@
-package com.eliab.controleestudos.repository;public class HistoricoRepository {
+package com.eliab.controleestudos.repository;
+
+import com.eliab.controleestudos.model.Historico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HistoricoRepository extends JpaRepository<Historico, Long> {
 }
--- backend/controleestudos/src/main/java/com/eliab/controleestudos/repository/UsuarioRepository.java
+++ backend/controleestudos/src/main/java/com/eliab/controleestudos/repository/UsuarioRepository.java
@@ -1,2 +1,9 @@
-package com.eliab.controleestudos.repository;public interface UsuarioRepository {
+package com.eliab.controleestudos.repository;
+
+import com.eliab.controleestudos.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
 }
