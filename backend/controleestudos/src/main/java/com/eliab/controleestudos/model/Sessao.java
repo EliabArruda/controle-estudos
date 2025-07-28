4d540ddbfbb1cb0c76350b7aad4ef73dcc2ba6d5
Eliab <eliabad20t@gmail.com>
2025-07-28 15:58:10 -0300

    Ajustes em entidades Historico e Sessao + Repository de Historico

--- backend/controleestudos/src/main/java/com/eliab/controleestudos/model/Sessao.java
+++ backend/controleestudos/src/main/java/com/eliab/controleestudos/model/Sessao.java
@@ -1,6 +1,9 @@
 package com.eliab.controleestudos.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -19,9 +22,15 @@ public class Sessao {
 
     private String disciplina;
     private String assunto;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dataInicio;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dataFim;
-    private int duracaoMinutos;
+
+    @Min(value = 25, message = "A duração mínima é de 25 minutos!")
+    @Max(value = 120, message = "A duração mínima é de 2 horas!")
+    private Integer duracaoMinutos;
     private StatusEnum status;
 }
 
